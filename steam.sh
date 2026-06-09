389443612caa652059de9c58a65f2b39e27a6b4b
Andres Prat <aprat.cl@gmail.com>
2026-06-09 15:37:28 -0400

    add steam launcher

---
+++ steam.sh
@@ -0,0 +1,3 @@
+dpkg-deb -x ./portal-armbian/portal-steam-bundle/libvpx6_1.8.2-1ubuntu0.4_arm64.deb /tmp/libvpx6
+ln -sf "$(find /tmp/libvpx6 -name 'libvpx.so.6*' | head -1)" ~/.local/share/Steam/lib/aarch64-linux-gnu/libvpx.so.6
+SDL_VIDEODRIVER=x11 GDK_BACKEND=x11 portal-steam --no-gamescope --gamepadui
